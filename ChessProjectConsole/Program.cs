c5525cb68ac08c02177c80b4c4c29126516f4a7e
Willian Braga <willian.kaeru@gmail.com>
2019-07-12 15:17:02 -0300

    Initial commit

---
+++ ChessProjectConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChessProjectConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
